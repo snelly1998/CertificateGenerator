cc674fb0a5224e3485d371f44b1c713bb07de881
Anthony Howard <AnthonyProbert17@gmail.com>
2023-03-22 11:54:14 -0300

    Created a check for company names and filter based on that. Creates new folders depending on comapny name now.

--- system.py
+++ system.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 import csv
+import os
 
 number_of_certificates = 0
 
@@ -9,8 +10,16 @@ with open('data.csv') as f:
     reader = csv.reader(f)
     data = list(reader)
 
-for i, name in enumerate(data):
+unique_values = []
 
+for i, name in enumerate(data):
+    if name[2] not in unique_values:
+        unique_values.append(name[2])
+
+for value in unique_values:
+    os.makedirs(f"./{value}/", exist_ok=True)
+
+for i, name in enumerate(data):
     image = Image.open('cert.png')
     draw = ImageDraw.Draw(image)
     text_width, text_height = draw.textsize(name[0], font)
@@ -30,23 +39,8 @@ for i, name in enumerate(data):
 
     print(f'./images/{name[0]} - {name[1]} - {name[2]}.jpg')
 
-    image.save(f'./images/{name[0]} - {name[1]} - {name[2]}.jpg')
+    image.save(f'./certs/{name[2]}/{name[0]} - {name[1]} - {name[2]}.jpg')
 
     number_of_certificates += 1
-
-    #Splitting the certificates into different folders. 
-
-    # if name[2] == 'Harnois':
-
-    #     image.save(f'./images/Harnois/{name[0]} - {name[1]} - {name[2]}.jpg')
-
-    #     number_of_certificates += 1
-    # else:
-
-    #     print(f'./images/{name[0]} - {name[1]} - {name[2]}.jpg')
-
-    #     image.save(f'./images/{name[0]} - {name[1]} - {name[2]}.jpg')
-
-    #     number_of_certificates += 1
 
 print(number_of_certificates)
