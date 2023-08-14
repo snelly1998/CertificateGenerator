5b1210f8d9c3adeec44d8befcf4bab388311b110
Jared Snell <snelly1998@gmail.com>
2023-08-14 10:44:52 -0300

    Required Score from "100.0" to "100"

--- system.py
+++ system.py
@@ -4,62 +4,68 @@ import os
 import zipfile
 from datetime import datetime
 
-# TODO: Rewrite the certificate.png so that we can add a Course Name rather than it being static. 
+# TODO: Rewrite the certificate.png so that we can add a Course Name rather than it being static.
 
-#Global Vars
-folder_path = './certs/'
-font = ImageFont.truetype('arial.ttf', 60)
+# Global Vars
+folder_path = "./certs/"
+font = ImageFont.truetype("arial.ttf", 60)
 unique_values = []
 start_date = "2023-05-08"
 checked_date = "2023-05-21"
-required_score = "100.0"
+required_score = "100"
 
-with open('data.csv', encoding="utf8", errors='ignore') as f:
+with open("data.csv", encoding="utf8", errors="ignore") as f:
     reader = csv.reader(f)
     data = list(reader)
 
-# Loops over the store names and adds each unique name into a list. 
+# Loops over the store names and adds each unique name into a list.
 for i, name in enumerate(data):
     if name[15] not in unique_values:
         unique_values.append(name[15])
 
-# Loops over the list and creates a folder for each of the unique store names in the ./certs/ folder. 
+# Loops over the list and creates a folder for each of the unique store names in the ./certs/ folder.
 for value in unique_values:
     os.makedirs(f"./certs/{value}/", exist_ok=True)
 
 for i, name in enumerate(data):
-    # Grab the information for each of the students and store them in a variable. 
+    # Grab the information for each of the students and store them in a variable.
     student = name[1]
     complete_date = name[8].split("T")[0]
     store_name = name[15]
     score = name[3]
-    
+
     # Checks to see if the date on the certificate is on or after the date provided by the user
-    # This ensures that the certificates being generated are only the ones that are required.   
-    if complete_date >= start_date and checked_date >= start_date and score == required_score:
+    # This ensures that the certificates being generated are only the ones that are required.
+    if (
+        complete_date >= start_date
+        and checked_date >= start_date
+        and score == required_score
+    ):
         # Grabs the image and draws it
-        image = Image.open('cert.png')
+        image = Image.open("cert.png")
         draw = ImageDraw.Draw(image)
         text_width, text_height = draw.textsize(student, font)
 
-        # Gets the center of the page so that we can write the students name in the middle. 
-        x = (image.width - text_width) / 2 
-        y = (image.height - text_height) / 2 
+        # Gets the center of the page so that we can write the students name in the middle.
+        x = (image.width - text_width) / 2
+        y = (image.height - text_height) / 2
         draw.text((x, y), student, font=font, fill=(0, 0, 0))
-        
+
         # Writes the Date
         draw.text((1250, 1000), complete_date, font=font, fill=(0, 0, 0))
-        
-        #Writes the Store Name
+
+        # Writes the Store Name
         draw.text((300, 1000), store_name, font=font, fill=(0, 0, 0))
 
-        # Saves the image to the certs folder. 
-        image.save(f'./certs/{store_name}/{student} - {complete_date} - {store_name}.jpg')
-        
+        # Saves the image to the certs folder.
+        image.save(
+            f"./certs/{store_name}/{student} - {complete_date} - {store_name}.jpg"
+        )
+
 for folder_name in os.listdir(folder_path):
     # Construct the full path to the folder
     folder_path_full = os.path.join(folder_path, folder_name)
-     # Make sure the item is a directory, not a file
+    # Make sure the item is a directory, not a file
     if os.path.isdir(folder_path_full):
         # Check if the folder contains any files
         if len(os.listdir(folder_path_full)) > 0:
@@ -71,7 +77,5 @@ for folder_name in os.listdir(folder_path):
                     file_path_full = os.path.join(folder_path_full, file_name)
                     if os.path.isfile(file_path_full):
                         zip_file.write(file_path_full, file_name)
-                        
-checked_date = str(datetime.today()).split(" ")[0]
 
-                
+checked_date = str(datetime.today()).split(" ")[0]
